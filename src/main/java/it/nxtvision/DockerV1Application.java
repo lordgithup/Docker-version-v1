b7c70e93e9f2ef55ac3bc0e83c2504c813e4dee3
Kul@7489 <nagarkuldeep002@gmail.com>
2025-04-12 17:47:15 +0530

    initial commiting

---
+++ src/main/java/it/nxtvision/DockerV1Application.java
@@ -0,0 +1,13 @@
+package it.nxtvision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerV1Application.class, args);
+	}
+
+}
---
+++ src/main/java/it/nxtvision/controller/DemoController.java
@@ -0,0 +1,14 @@
+package it.nxtvision.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from Dockerized Spring Boot!";
+    }
+}
